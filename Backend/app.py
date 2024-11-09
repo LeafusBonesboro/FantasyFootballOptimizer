c821e43279c748915fc843eb908c957578be2354
LeafusBonesboro <108142251+LeafusBonesboro@users.noreply.github.com>
2024-11-09 06:18:57 -0500

    Add site-specific login and signup functionality

--- Backend/app.py
+++ Backend/app.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template
+from flask import render_template, Flask
 import os
 import secrets
 from routes.auth import auth_bp
 from routes.leagues import leagues_bp
 from routes.teams import teams_bp
-from flask_cors import CORS
+from config import app, db
+
+from routes.local_auth import local_auth_bp
 
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 generated_secret_key = secrets.token_hex(24)  # Generates a random 24-byte hexadecimal string
-app = Flask(__name__)
-CORS(app)
+
 
 
 # Use a persistent secret key (preferably from environment variable)
@@ -24,10 +25,15 @@ app.register_blueprint(auth_bp, url_prefix='/auth')  # Add the /auth pre
 app.register_blueprint(leagues_bp)
 app.register_blueprint(teams_bp, url_prefix='/teams')  # Add a prefix for API routes
 
+app.register_blueprint(local_auth_bp, url_prefix='/local_auth')
+
 @app.route('/')
 def home():
     return render_template('base.html')
 
 if __name__ == '__main__':
-    app.run
+    with app.app_context():
+        db.create_all()  # Create tables if they don't exist
+
+    app.run(debug=True)
 # .\ngrok http 5000
